7e3a722062dbac80194ead16c52a6c366f8f0058
Gleb <unicode256@yandex.by>
2023-10-07 20:35:16 +0300

    change base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/vape-free',
+    path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '/',
+        path: '',
         name: 'Main',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
